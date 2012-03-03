b2bb0ef30386d60c3ee889fc10271b7119e73da6
vaclav <vaclav@6e3d5a94-014d-4e71-8e65-48e4780539f1>
2012-03-03 20:11:59 +0000

    add doc for msvc, fix cmake build on linux
    git-svn-id: https://yborm.svn.sourceforge.net/svnroot/yborm/trunk@93 6e3d5a94-014d-4e71-8e65-48e4780539f1

--- tutorial/tut1.cpp
+++ tutorial/tut1.cpp
@@ -6,23 +6,29 @@ using namespace std;
 
 int main()
 {
-    auto_ptr<Yb::SqlConnect> conn(
-        new Yb::SqlConnect("ODBC", "MYSQL", "test1_db", "test1", "test1_pwd"));
-    Yb::Engine engine(Yb::Engine::MANUAL, conn);
-    Yb::Session session(Yb::init_default_meta(), &engine);
+    try {
+        auto_ptr<Yb::SqlConnect> conn(
+            new Yb::SqlConnect("ODBC", "INTERBASE", "test1_db_fb", "test1", "test1_pwd"));
+        Yb::Engine engine(Yb::Engine::MANUAL, conn);
+        Yb::Session session(Yb::init_default_meta(), &engine);
 
-    Domain::Client client;
-    string name, email, budget;
-    cout << "Enter name, email, budget:\n";
-    cin >> name >> email >> budget;
-    client.set_name(name);
-    client.set_email(email);
-    client.set_budget(Yb::Decimal(budget));
-    client.set_dt(Yb::now());
-    client.save(session);
-    session.flush();
-    cout << "New client: " << client.get_id() << endl;
-    engine.commit();
-    return 0;
+        Domain::Client client;
+        string name, email, budget;
+        cout << "Enter name, email, budget:\n";
+        cin >> name >> email >> budget;
+        client.set_name(name);
+        client.set_email(email);
+        client.set_budget(Yb::Decimal(budget));
+        client.set_dt(Yb::now());
+        client.save(session);
+        session.flush();
+        cout << "New client: " << client.get_id() << endl;
+        engine.commit();
+        return 0;
+    }
+    catch (const std::exception &ex) {
+        cout << "exception: " << ex.what() << endl;
+        return 1;
+    }
 }
-
+ 
