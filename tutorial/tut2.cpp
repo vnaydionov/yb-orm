94224644a045743adfb40f40e3fbfcddb1e07a57
Viacheslav Naydenov <vaclav@yandex.ru>
2012-07-04 16:18:27 +0400

    fix tutorial2

--- tutorial/tut2.cpp
+++ tutorial/tut2.cpp
@@ -7,30 +7,38 @@ using namespace std;
 
 int main()
 {
-    auto_ptr<Yb::SqlConnect> conn(
-        new Yb::SqlConnect("ODBC", "MYSQL", "test1_db", "test1", "test1_pwd"));
-    Yb::Engine engine(Yb::Engine::MANUAL, conn);
-    Yb::Session session(Yb::init_default_meta(), &engine);
+    try {
+        auto_ptr<Yb::SqlConnection> conn(
+            new Yb::SqlConnection("ODBC",
+                "MYSQL", "test1_db", "test1", "test1_pwd"));
+        Yb::Engine engine(Yb::Engine::MANUAL, conn);
+        Yb::Session session(Yb::init_default_meta(), &engine);
 
-    Domain::Order order;
-    string amount;
-    cout << "Enter order amount: \n";
-    cin >> amount;
-    order.set_total_sum(Yb::Decimal(amount));
-    order.save(session);
+        Domain::Order order;
+        string amount;
+        cout << "Enter order amount: \n";
+        cin >> amount;
+        order.total_sum = Yb::Decimal(amount);
+        order.save(session);
 
-    Domain::Client client;
-    string name, email;
-    cout << "Enter name, email: \n";
-    cin >> name >> email;
-    client.set_name(name);
-    client.set_email(email);
-    client.set_dt(Yb::now());
-    client.save(session);
+        Domain::ClientHolder client;
+        string name, email;
+        cout << "Enter name, email: \n";
+        cin >> name >> email;
+        client->name = name;
+        client->email = email;
+        client->dt = Yb::now();
+        client->save(session);
 
-    order.set_owner(client);
-    session.flush();
-    engine.commit();
-    cout << order.xmlize(1).get_xml() << endl;
-    return 0;
+        cout << "Client's orders count: " << client->orders.size() << "\n";
+        order.owner = client;
+        cout << "Client's orders count: " << client->orders.size() << "\n";
+        session.commit();
+        cout << order.xmlize(1)->serialize() << endl;
+        return 0;
+    }
+    catch (const std::exception &ex) {
+        cout << "exception: " << ex.what() << endl;
+        return 1;
+    }
 }
