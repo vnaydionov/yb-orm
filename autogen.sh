76a3422f951eae6a9c247be6366cd4983d547515
Nikita Buyvich <buyvich@gmail.com>
2013-09-03 11:24:19 +0400

    initial. small fix to mac build

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-libtoolize --automake --copy --force &&
+if hash libtoolize 2>&-
+then
+    libtoolize --automake --force
+else
+    glibtoolize --automake --force
+fi
 autoheader &&
 rm -rf aclocal.m4 &&
 aclocal &&
