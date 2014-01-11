9e12474f69134abd908dd335b5c1023d8e44e056
Viacheslav Naydenov <vnaydionov@gmail.com>
2014-01-11 06:41:23 +0400

    fix paths in build .bat files

--- examples/tutorial/build-mingw.bat
+++ examples/tutorial/build-mingw.bat
@@ -2,5 +2,5 @@ set BAT_DIR=%~dp0
 call %BAT_DIR%..\bin\yborm_env.bat
 
 set BAT_DIR=%~dp0
-cmake -G "MinGW Makefiles" -D CMAKE_INSTALL_PREFIX:PATH=%BAT_DIR%.. -D YBORM_ROOT:PATH=%BAT_DIR%.. %BAT_DIR%..\src\tutorial
+cmake -G "MinGW Makefiles" -D CMAKE_INSTALL_PREFIX:PATH=%BAT_DIR%.. -D YBORM_ROOT:PATH=%BAT_DIR%.. %BAT_DIR%..\src\examples\tutorial
 mingw32-make && mingw32-make install
