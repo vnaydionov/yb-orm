1b47852a5e1f3a06db499a2356ccd0d5b9eae8b8
Viacheslav Naydenov <vnaydionov@gmail.com>
2014-07-17 09:33:00 +0400

    keep .md files in GitHub wiki-repo

--- doc/update_md.sh
+++ doc/update_md.sh
@@ -1,8 +1,17 @@
 
 for x in *.en.html ; do
-    y=`echo -n $x | sed 's/[.]en[.]html//'`
+    y=`echo -n $x | sed 's/[.]en[.]html/.md/'`
     echo $x $y
     html2text.py $x > $y
-    sed -i 's/[.]\(en\|ru\)[.]html//g' $y
+    sed -i 's/[.]en[.]html//g' $y
+    sed -i 's+pics/+https://raw.githubusercontent.com/vnaydionov/yb-orm/master/doc/pics/+g' $y
+done
+
+for x in *.ru.html ; do
+    y=`echo -n $x | sed 's/\(.*\)[.]ru[.]html/ru_\1.md/'`
+    echo $x $y
+    html2text.py $x > $y
+    sed -i 's/\(.*\)[.]ru[.]html/ru_\1.md/g' $y
+    sed -i 's+pics/+https://raw.githubusercontent.com/vnaydionov/yb-orm/master/doc/pics/+g' $y
 done
 
