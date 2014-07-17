d8dbf7f59e3ba3c8a9086cd973e0f90f43045b1f
Viacheslav Naydenov <vnaydionov@gmail.com>
2014-07-17 10:25:40 +0400

    add scripts for updating .md files; fix links

--- doc/update_md.sh
+++ doc/update_md.sh
@@ -1,17 +1,8 @@
 
-for x in *.en.html ; do
-    y=`echo -n $x | sed 's/[.]en[.]html/.md/'`
+for x in *.en.html *.ru.html ; do
+    y=`echo -n $x | sed 's/\(.*\)[.]\(en\|ru\)[.]html/\2_\1.md/'`
     echo $x $y
     html2text.py $x > $y
-    sed -i 's/[.]en[.]html//g' $y
+    sed -i 's/\([A-Za-z0-9]*\)[.]\(en\|ru\)[.]html/\2_\1/g' $y
     sed -i 's+pics/+https://raw.githubusercontent.com/vnaydionov/yb-orm/master/doc/pics/+g' $y
 done
-
-for x in *.ru.html ; do
-    y=`echo -n $x | sed 's/\(.*\)[.]ru[.]html/ru_\1.md/'`
-    echo $x $y
-    html2text.py $x > $y
-    sed -i 's/\(.*\)[.]ru[.]html/ru_\1.md/g' $y
-    sed -i 's+pics/+https://raw.githubusercontent.com/vnaydionov/yb-orm/master/doc/pics/+g' $y
-done
-
